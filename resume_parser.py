15945ff3334145a9cae136d78d3d65d3093bd22f
HETARTH21 <hetarthchauhan21@gmail.com>
2026-01-06 23:42:50 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -6,4 +6,20 @@ def extract_text_from_pdf(pdf_path):
         reader = PyPDF2.PdfReader(file)
         for page in reader.pages:
             text += page.extract_text()
-    return text
+    return text.lower()
+
+def extract_skills(text):
+    skills = [
+        "python", "java", "c++", "sql",
+        "machine learning", "data analysis",
+        "nlp", "flask", "git", "linux"
+    ]
+
+    text = text.lower()
+    found = []
+
+    for skill in skills:
+        if skill in text:
+            found.append(skill)
+
+    return found
